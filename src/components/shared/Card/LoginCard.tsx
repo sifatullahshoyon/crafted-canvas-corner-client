b12e3011a47af93c667fed163d71ac2e0915aee8
freelancersifat627e <freelancersifat627@gmail.comm>
2025-02-27 15:29:18 +0600

    login form update

--- src/components/shared/Card/LoginCard.tsx
+++ src/components/shared/Card/LoginCard.tsx
@@ -1,10 +1,8 @@
-import LoginCheckbox from "@/components/checkbox/LoginCheckbox";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -12,8 +10,36 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 import Divider from "../Divider";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import LoginCheckbox from "@/components/checkbox/LoginCheckbox";
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string(),
+});
 
 const LoginCard = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="w-full h-full flex flex-col justify-center shadow-none overflow-hidden rounded">
       <Card className="md:w-4/5 h-full mx-auto flex flex-col justify-center shadow-none overflow-hidden rounded border-none">
@@ -29,30 +55,52 @@ const LoginCard = () => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="email" className="font-roboto">
-                  Email
-                </Label>
-                <Input
-                  id="email"
-                  name="password"
-                  placeholder="Enter Your Email"
-                  className="font-roboto"
-                />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="font-roboto">
-                  Password
-                </Label>
-                <Input
-                  id="password"
-                  name="password"
-                  placeholder="Enter Your Password"
-                  className="font-roboto"
-                />
-              </div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid w-full items-center gap-4"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label htmlFor="email" className="font-roboto">
+                      Email
+                    </Label>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        placeholder="Enter Your Email"
+                        {...field}
+                        className="font-roboto"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label htmlFor="password" className="font-roboto">
+                      Password
+                    </Label>
+                    <FormControl>
+                      <Input
+                        id="password"
+                        type="password"
+                        placeholder="Enter Your Password"
+                        {...field}
+                        className="font-roboto"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <div className="flex flex-col md:flex-row justify-between items-center space-y-1.5">
                 <LoginCheckbox />
                 <Link
@@ -62,29 +110,23 @@ const LoginCard = () => {
                   Forgot Password
                 </Link>
               </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-center">
-          <Button className="w-full font-roboto bg-[#EF6291] hover:bg-[#EF6291] hover:text-[#1A1A1A]">
-            Sign In
-          </Button>
-        </CardFooter>
-        <CardContent>
-          <div className="flex flex-col space-y-1.5">
-            <Divider />
-          </div>
-          <div className="flex flex-col space-y-1.5">
-            <p className="font-roboto text-xs text-[#1A1A1A] text-center mt-2">
-              Don't have an account? Please{" "}
-              <Link
-                to="/registration"
-                className="text-[#EF6291] hover:underline"
+              <Button
+                type="submit"
+                className="w-full font-roboto bg-[#EF6291] hover:bg-[#EF6291] hover:text-[#1A1A1A]"
               >
-                Sign up
-              </Link>
-            </p>
-          </div>
+                Sign In
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardContent>
+          <Divider />
+          <p className="font-roboto text-xs text-[#1A1A1A] text-center mt-2">
+            Don't have an account? Please{" "}
+            <Link to="/registration" className="text-[#EF6291] hover:underline">
+              Sign up
+            </Link>
+          </p>
         </CardContent>
       </Card>
     </div>
